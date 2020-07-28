b7010477f996e5328fbe3753ee01edee62f5ec9e
leaf_eecs <leafeecs@gmail.com>
2020-07-28 20:21:25 +0900

    Add: Selection Sort Algorithm

---
+++ algorithms/sorting/selection_sort.py
@@ -0,0 +1,15 @@
+import random
+
+def selection_sort(data):
+    for stand_index in range(len(data) - 1):
+        lowest_index = stand_index
+        for index in range(stand_index + 1, len(data)):
+            if data[lowest_index] > data[index]:
+                lowest_index = index
+        data[stand_index], data[lowest_index] = data[lowest_index], data[stand_index]
+
+    return data
+
+data_list = random.sample(range(100), 10)
+print(data_list)                 # [22, 91, 15, 25, 49, 4, 19, 87, 32, 52]
+print(selection_sort(data_list)) # [4, 15, 19, 22, 25, 32, 49, 52, 87, 91]
